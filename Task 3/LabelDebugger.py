03924853bed890c0eb48b0825e2b7acb897a63a6
Bauyrzhan Zhanuzakov <83038196+ZhanB-Z@users.noreply.github.com>
2023-12-19 07:26:03 +0100

    Update LabelDebugger.py

--- Task 3/LabelDebugger.py
+++ Task 3/LabelDebugger.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree as ET
 from Masks_Updated import load_masks_from_xml
 
 images_path = "/Users/zhan/Desktop/Python/Task 3/images"
-save_path = "/Users/zhan/Desktop/Python/Task 3/modified_images"
+save_path = "/Users/zhan/Desktop/Python/Task 3/debug_images"
 
 xml_file_path = "/Users/zhan/Desktop/Python/Task 3/masks.xml"
 masks_data = load_masks_from_xml(xml_file_path)
@@ -24,15 +24,16 @@ def debug_ignore(image_path, masks, save_path):
     debug_image.save(os.path.join(save_path, f'debug_{file_name}.png'))
     print(f"Success, {file_name} was created")
 
-for image_file in os.listdir(images_path):
-    image_path = os.path.join(images_path, image_file)
-    if not image_file.lower().endswith(('.png','jpg')):
-        print(f"Skipping non-image file: {image_file}")
-        continue
-
-    image = Image.open(image_path)
-    file_name = os.path.splitext(os.path.basename(image_path))[0]
-
-    if image_file in masks_data:
-        debug_ignore(image_path, masks_data[image_file], save_path)
+if __name__ == "__main__":
+    for image_file in os.listdir(images_path):
+        image_path = os.path.join(images_path, image_file)
+        if not image_file.lower().endswith(('.png','jpg')):
+            print(f"Skipping non-image file: {image_file}")
+            continue
+    
+        image = Image.open(image_path)
+        file_name = os.path.splitext(os.path.basename(image_path))[0]
+    
+        if image_file in masks_data:
+            debug_ignore(image_path, masks_data[image_file], save_path)
         
